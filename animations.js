9cccc3637d3263f9bcb8280e7ccced5f98526246
Cornell van der Straaten <30183@ma-web.nl>
2020-11-27 12:23:09 +0100

    Stap 5 af

--- animations.js
+++ animations.js
@@ -2,3 +2,8 @@
 document.querySelector('.animate-button').addEventListener('click', (e) => {
     document.querySelector('.square').classList.toggle('square__to-right')
 })
+
+// After some time of loading page, start animation automatically
+setTimeout( () => {
+    document.querySelector('.square').classList.toggle('square__to-right');
+}, 2000)
