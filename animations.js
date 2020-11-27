6605dcea01af44fcf842c9e9e867115645195b42
Cornell van der Straaten <30183@ma-web.nl>
2020-11-27 12:36:19 +0100

    stap 6 af

--- animations.js
+++ animations.js
@@ -7,3 +7,14 @@ document.querySelector('.animate-button').addEventListener('click', (e) => {
 setTimeout( () => {
     document.querySelector('.square').classList.toggle('square__to-right');
 }, 2000)
+
+// Clicking on a nav button pushes every nav__link on the page by giving them the class nav__link--push-in
+const menuButton        = document.querySelector('.nav__button');
+const navigationItems   = document.querySelectorAll('.nav__link');
+menuButton.addEventListener('click', () => {
+    navigationItems.forEach( (item,index) => {
+        setTimeout( () => {
+            item.classList.toggle('nav__link--push-in');
+        }, 100 * index);
+    })
+})
